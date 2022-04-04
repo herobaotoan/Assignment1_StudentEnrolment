9181a0933bbc9beb2be0444ba974664ccc68ff6c
Chi Toan <88176211+herobaotoan@users.noreply.github.com>
2022-04-04 23:20:11 +0700

    Update Student.java
    Remove course ArrayList

--- Student.java
+++ Student.java
@@ -1,10 +1,7 @@
-import java.util.ArrayList;
-
 public class Student {
     private String ID;
     private String name;
     private String birthday;
-    private ArrayList<Course> coursesList;
 
     public Student(String ID, String name, String birthday) {
         this.ID = ID;
@@ -24,15 +21,10 @@ public class Student {
         return birthday;
     }
 
-    public void addCourse(Course course) {
-        coursesList.add(course);
-    }
-
     @java.lang.Override
     public java.lang.String toString() {
         return "Student{" +
-                "ID='" + ID + '\'' +
-                ", name='" + name + '\'' +
-                ", birthday='" + birthday + '\'';
+                 ID + '\'' +
+                 name + '\'';
     }
 }
