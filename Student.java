83909da62446033057ac9f9a11b92885574d3946
Chi Toan <88176211+herobaotoan@users.noreply.github.com>
2022-04-04 23:21:20 +0700

    Update Student.java
    Add Birthday in toString

--- Student.java
+++ Student.java
@@ -25,6 +25,7 @@ public class Student {
     public java.lang.String toString() {
         return "Student{" +
                  ID + '\'' +
-                 name + '\'';
+                 name + '\'' +
+                 birthday + '}';
     }
 }
